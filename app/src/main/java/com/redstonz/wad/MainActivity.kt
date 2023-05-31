3290f8e7e1b5f1c3feeb8027fbc9f9218e3961b2
Adnan <adnan@metricapp.co>
2023-05-31 12:19:06 +0500

    whatsapp business added

--- app/src/main/java/com/redstonz/wad/MainActivity.kt
+++ app/src/main/java/com/redstonz/wad/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.redstonz.wad
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.redstonz.wad.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -13,17 +16,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.ccp.registerCarrierNumberEditText(binding.number)
-        binding.button.setOnClickListener {
+        binding.whatsapp.setOnClickListener {
 
             val number = binding.ccp.fullNumberWithPlus
 
-            if(binding.ccp.isValidFullNumber)
-            binding.webview.loadUrl("http://wa.me/$number")
-else
+            if (binding.ccp.isValidFullNumber)
+                openWhatsApp(number)
+            else
                 Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_SHORT).show()
 
         }
 
 
     }
+
+
+    fun openWhatsApp(number: String) {
+        try {
+            val url = "whatsapp://send?phone=$number"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
 }
